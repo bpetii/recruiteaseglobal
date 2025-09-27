50c3e9f76e93e2f13c596da4e3c1afcd86042d92
Peter Biro <peter.biro@oliasoft.com>
2025-09-27 16:36:19 +0200

    try dev

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
